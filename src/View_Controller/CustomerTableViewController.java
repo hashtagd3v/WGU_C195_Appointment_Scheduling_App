bcc850837bb5b13490c4443e4294e52fb9c3ab89
Jay M <jaycoinminer@gmail.com>
2020-12-30 00:24:55 -0600

    Created Customer Table View List screen.

---
+++ src/View_Controller/CustomerTableViewController.java
@@ -0,0 +1,35 @@
+package View_Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CustomerTableViewController {
+
+    public TextField customerListSearchText;
+    public TableView customerTableView;
+    public TableColumn customerIdCol;
+    public TableColumn customerNameCol;
+    public TableColumn customerAddressCol;
+    public TableColumn customerFirstLevelDivCol;
+    public TableColumn customerPostalCodeCol;
+    public TableColumn customerCountryCol;
+
+
+    public void onActionCustomerBackBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionCustomerAddBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionCustomerUpdateBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionCustomerDeleteBtn(ActionEvent actionEvent) {
+
+    }
+}
