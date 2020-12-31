4d067373c77012dfb0f534728a4d383b9a0c809f
Jay M <jaycoinminer@gmail.com>
2020-12-30 23:52:53 -0600

    Created Add Customer Screen

---
+++ src/View_Controller/AddCustomerController.java
@@ -0,0 +1,24 @@
+package View_Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class AddCustomerController {
+    
+    public TextField addCustomerIDText;
+    public TextField addCustomerNameText;
+    public TextField addCustomerAddressText;
+    public ComboBox addCustomerFirstLdCombo;
+    public ComboBox addCustomerCountryCombo;
+    public TextField addCustomerPostalText;
+    public TextField addCustomerPhoneText;
+
+    public void onActionAddCustomerCancelBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionAddCustomerAddBtn(ActionEvent actionEvent) {
+
+    }
+}
