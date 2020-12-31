7535bf503b4bbb6dcef56af02bca923e98477552
Jay M <jaycoinminer@gmail.com>
2020-12-31 00:00:29 -0600

    Created Update Customer Screen

--- src/View_Controller/UpdateCustomerController.java
+++ src/View_Controller/UpdateCustomerController.java
@@ -1,4 +1,29 @@
 package View_Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class UpdateCustomerController {
+
+    public TextField updateIDText;
+    public TextField updateNameText;
+    public TextField updateAddressText;
+    public ComboBox updateFirstLdCombo;
+    public ComboBox updateCountryCombo;
+    public TextField updatePostalText;
+    public TextField updatePhoneText;
+
+    public void onActionUpdateCustomerCancelBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionUpdateCustomerDeleteBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionUpdateCustomerBtn(ActionEvent actionEvent) {
+        
+    }
+
 }
