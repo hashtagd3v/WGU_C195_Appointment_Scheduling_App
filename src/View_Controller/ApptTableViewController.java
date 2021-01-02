e9a7376dde04de2351585cf39721e2d8b51e589c
Jay M <jaycoinminer@gmail.com>
2021-01-02 01:04:07 -0600

    Created Appointment View/Filter Screen

---
+++ src/View_Controller/ApptTableViewController.java
@@ -0,0 +1,46 @@
+package View_Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ApptTableViewController {
+    
+    public TextField apptListSearchText;
+    public TableView apptTableView;
+    public TableColumn apptIdCol;
+    public TableColumn apptTitleCol;
+    public TableColumn apptDescriptionCol;
+    public TableColumn apptLocationCol;
+    public TableColumn apptContactCol;
+    public TableColumn apptTypeCol;
+    public TableColumn apptStartDateTimeCol;
+    public TableColumn apptEndDateTimeCol;
+    public TableColumn apptCustomerIDCol;
+
+    public void onActionFilterByMonthBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionFilterByWeekBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionApptBackBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionApptAddBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionApptUpdateBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionApptDeleteBtn(ActionEvent actionEvent) {
+
+    }
+
+}
