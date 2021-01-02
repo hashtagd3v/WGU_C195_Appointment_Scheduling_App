aa377835221a255a173c59be76ae6d7f3458a7fe
Jay M <jaycoinminer@gmail.com>
2021-01-02 00:31:52 -0600

    Created Update Appointment Screen

---
+++ src/View_Controller/UpdateAppointmentController.java
@@ -0,0 +1,40 @@
+package View_Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class UpdateAppointmentController {
+
+    public TextField updateApptIDText;
+    public TextField updateApptTypeText;
+    public TextField updateApptTitleText;
+    public TextField updateApptDescriptionText;
+    public TextField updateApptLocationText;
+    public ComboBox updateApptContactCombo;
+    public DatePicker updateApptStartDatePicker;
+    public Spinner updateApptStartHour;
+    public Spinner updateApptStartMin;
+    public Spinner updateApptStartAmPm;
+    public DatePicker updateApptEndDatePicker;
+    public Spinner updateApptEndHour;
+    public Spinner updateApptEndMin;
+    public Spinner updateApptEndAmPm;
+    public TextField updateApptCustomerIDText;
+    public TextField updateApptUserIDText;
+
+    public void onActionUpdateApptCancelBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionUpdateApptDeleteBtn(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionUpdateApptBtn(ActionEvent actionEvent) {
+
+    }
+    
+}
