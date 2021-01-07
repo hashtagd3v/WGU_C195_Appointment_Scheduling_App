0ceb75eadf6ec7ccd5e67f750afec333a7d53cb3
Jay M <jaycoinminer@gmail.com>
2021-01-06 23:31:03 -0600

    Added event handlers for WelcomeController.java. Also added javadoc comments for class & methods in this class.

--- src/View_Controller/CustomerTableViewController.java
+++ src/View_Controller/CustomerTableViewController.java
@@ -1,10 +1,19 @@
 package View_Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+/** Customer Table View Screen shows a table view list of customers in the database. Users are able
+ * to add, update and delete a customer here.*/
 public class CustomerTableViewController {
 
     public TextField customerListSearchText;
@@ -16,20 +25,42 @@ public class CustomerTableViewController {
     public TableColumn customerPostalCodeCol;
     public TableColumn customerCountryCol;
 
+    Stage stage;
+    Parent scene;
 
-    public void onActionCustomerBackBtn(ActionEvent actionEvent) {
+    /** Goes back to Welcome screen when clicked.*/
+    public void onActionCustomerBackBtn(ActionEvent actionEvent) throws IOException {
+
+        stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/View_Controller/Welcome.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+
+    }
+    /** When button is clicked, opens up Add Customer screen.*/
+    public void onActionCustomerAddBtn(ActionEvent actionEvent) throws IOException {
+
+        stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/View_Controller/AddCustomerScreen.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
 
     }
 
-    public void onActionCustomerAddBtn(ActionEvent actionEvent) {
+    /** When button is clicked, opens up Update Customer screen. This enables user to update information
+     * for customer selected in table view list.*/
+    public void onActionCustomerUpdateBtn(ActionEvent actionEvent) throws IOException {
+
+        stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/View_Controller/UpdateCustomerScreen.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
 
     }
 
-    public void onActionCustomerUpdateBtn(ActionEvent actionEvent) {
-
-    }
-
+    /** When button is clicked, this will enable user to delete selected customer from table view list.*/
     public void onActionCustomerDeleteBtn(ActionEvent actionEvent) {
 
     }
+
 }
