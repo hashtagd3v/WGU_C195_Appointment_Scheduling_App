b745bc3c1986a86b4b55fd7cb352840bffee7f2a
Jay M <jaycoinminer@gmail.com>
2021-01-07 00:40:00 -0600

    Added event handlers for buttons only UpdateAppointmentController.java Also added javadoc comments for class & methods in this class. Fixed UI for Update appointment screen: converted text fields for Customer & User ID, and appt type to combo boxes; deleted end date picker.

--- src/View_Controller/UpdateAppointmentController.java
+++ src/View_Controller/UpdateAppointmentController.java
@@ -1,39 +1,68 @@
 package View_Controller;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+/** Update Appointment screen enables user to edit/update information for appointment that was selected from
+ * appointment table view list. This screen will show a form with text fields for title, description and
+ * location of the appointment. It also has combo boxes for customer ID, user ID, type of appointment and contact
+ * assigned for appointment where user is able to make choices from list. This screen also has a date picker
+ * where user is able to select appointment date, and spinner boxes for start & end times for the appointment.*/
 public class UpdateAppointmentController {
 
     public TextField updateApptIDText;
-    public TextField updateApptTypeText;
     public TextField updateApptTitleText;
     public TextField updateApptDescriptionText;
     public TextField updateApptLocationText;
     public ComboBox updateApptContactCombo;
-    public DatePicker updateApptStartDatePicker;
     public Spinner updateApptStartHour;
     public Spinner updateApptStartMin;
-    public Spinner updateApptStartAmPm;
-    public DatePicker updateApptEndDatePicker;
     public Spinner updateApptEndHour;
     public Spinner updateApptEndMin;
-    public Spinner updateApptEndAmPm;
-    public TextField updateApptCustomerIDText;
-    public TextField updateApptUserIDText;
+    public ComboBox updateApptCustomerIDCombo;
+    public ComboBox updateApptUserIDCombo;
+    public ComboBox updateApptTypeCombo;
+    public DatePicker updateApptDatePicker;
 
-    public void onActionUpdateApptCancelBtn(ActionEvent actionEvent) {
+    Stage stage;
+    Parent scene;
+
+    /** Cancel button takes user back to previous screen: Appointment Table View list.*/
+    public void onActionUpdateApptCancelBtn(ActionEvent actionEvent) throws IOException {
+
+        stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/View_Controller/ApptTableView.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
 
     }
 
-    public void onActionUpdateApptDeleteBtn(ActionEvent actionEvent) {
+    /** Delete button enables user to delete appointment information shown on screen. Also takes user back
+     * to previous screen: Appointment Table View list.*/
+    public void onActionUpdateApptDeleteBtn(ActionEvent actionEvent) throws IOException {
+
+        stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/View_Controller/ApptTableView.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
 
     }
 
-    public void onActionUpdateApptBtn(ActionEvent actionEvent) {
+    /** Update button allows user to edit/update appointment information that was previously scheduled and is
+     * shown on screen. Also takes user back to previous screen: Appointment Table View list with new updated
+     * appointment information saved by user.*/
+    public void onActionUpdateApptBtn(ActionEvent actionEvent) throws IOException {
+
+        stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/View_Controller/ApptTableView.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
 
     }
 
