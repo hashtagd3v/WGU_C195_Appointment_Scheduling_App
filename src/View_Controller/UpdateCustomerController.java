acc4323a723022bbb2fe4771c9af3c667d1ecb6e
Jay M <jaycoinminer@gmail.com>
2021-01-06 23:46:11 -0600

    Added event handlers for UpdateCustomerController.java Also added javadoc comments for class & methods in this class.

--- src/View_Controller/UpdateCustomerController.java
+++ src/View_Controller/UpdateCustomerController.java
@@ -1,8 +1,15 @@
 package View_Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UpdateCustomerController {
 
@@ -14,15 +21,40 @@ public class UpdateCustomerController {
     public TextField updatePostalText;
     public TextField updatePhoneText;
 
-    public void onActionUpdateCustomerCancelBtn(ActionEvent actionEvent) {
+    Stage stage;
+    Parent scene;
+
+    /** Cancel button enables user to cancel and switch back to previous screen: Customers
+     * Table View list.*/
+    public void onActionUpdateCustomerCancelBtn(ActionEvent actionEvent) throws IOException {
+
+        stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/View_Controller/CustomerTableView.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
 
     }
 
-    public void onActionUpdateCustomerDeleteBtn(ActionEvent actionEvent) {
+    /** Delete button enables user to delete current user information shown on screen. This will also
+     * switch back to previous screen: Customers Table View List.*/
+    public void onActionUpdateCustomerDeleteBtn(ActionEvent actionEvent) throws IOException {
+
+        stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/View_Controller/CustomerTableView.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
 
     }
 
-    public void onActionUpdateCustomerBtn(ActionEvent actionEvent) {
+    /** Update button enables user to submit edited/updated customer information to the database. This will
+     * also switch user back to previous screen: Customer Table View list but with a list updated with
+     * the new customer information updated by user.*/
+    public void onActionUpdateCustomerBtn(ActionEvent actionEvent) throws IOException {
+
+        stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/View_Controller/CustomerTableView.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
 
     }
 
